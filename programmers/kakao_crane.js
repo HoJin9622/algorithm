90d6bd3ac301a813b19332e62defd19523f6214a
hojin9622 <kiss0104040@gmail.com>
2020-08-16 02:42:33 +0900

    프로그래머스 크레인 인형뽑기 게임
    Level 1
    JavaScript
    2019 카카오 개발자 겨울 인턴십

---
+++ programmers/kakao_crane.js
@@ -0,0 +1,24 @@
+function solution(board, moves) {
+  var answer = 0;
+  let arr = [];
+
+  for (let i = 0; i < moves.length; i++) {
+    for (let j = 0; j < board.length; j++) {
+      if (board[j][moves[i] - 1] !== 0) {
+        arr.push(board[j][moves[i] - 1]);
+        board[j][moves[i] - 1] = 0;
+        break;
+      }
+    }
+  }
+
+  for (let x = 0; x < arr.length; x++) {
+    if (arr[x] === arr[x + 1]) {
+      answer += 2;
+      arr.splice(x, 2);
+      x = -1;
+    }
+  }
+
+  return answer;
+}
