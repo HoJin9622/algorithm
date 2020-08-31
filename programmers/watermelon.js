8b8453d1e91c686e39e0ff91ad9be32050d1d94d
hojin9622 <kiss0104040@gmail.com>
2020-08-31 22:56:01 +0900

    프로그래머스 수박수박수박수박수박수박?
    Level 1
    JavaScript
    연습문제

---
+++ programmers/watermelon.js
@@ -0,0 +1,10 @@
+function solution(n) {
+  let answer = "";
+  let check = true;
+  while (n > 0) {
+    check ? (answer = answer.concat("수")) : (answer = answer.concat("박"));
+    check = !check;
+    n--;
+  }
+  return answer;
+}
